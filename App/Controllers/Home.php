8622dd90675c5669d724c25e5f30e389d5083e23
Koyozumii <jaumaquila@gmail.com>
2020-11-06 18:33:40 -0300

    login wip

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -3,34 +3,54 @@
 namespace App\Controllers;
 
 use \App\Core\Controller;
+use \App\Models\Dao\DaoAlunos;
 
 
-class Home extends Controller{
-    
-    public function index() {
+class Home extends Controller
+{
 
-    //    $dao = $this->model('DaoArtigos');
+    public function index()
+    {
 
-    //   $total = $dao->Total();
+        //    $dao = $this->model('DaoArtigos');
 
-    //$registros = 2;
+        //   $total = $dao->Total();
 
-
-    // $numPaginas = ceil($total/$registros);
-
-    // $inicio = ($registros*$pagina  )-$registros;
+        //$registros = 2;
 
 
-    //  $artigo = $dao->BuscarPorCOD(1);
-    //  $artigos = $dao->BuscarTodosPaginacao($inicio, $registros);
+        // $numPaginas = ceil($total/$registros);
 
-    //   $this->view->artigo = $artigo;
-    //  $this->view->artigos = $artigos;
-    //  $this->view->numPaginas = $numPaginas ;
+        // $inicio = ($registros*$pagina  )-$registros;
+
+
+        //  $artigo = $dao->BuscarPorCOD(1);
+        //  $artigos = $dao->BuscarTodosPaginacao($inicio, $registros);
+
+        //   $this->view->artigo = $artigo;
+        //  $this->view->artigos = $artigos;
+        //  $this->view->numPaginas = $numPaginas ;
         $this->render('index');
-
     }
 
-    
-    
+    public function logar()
+    {
+        /** @var DaoAlunos $dao */
+        $dao = $this->model('DaoAlunos');
+
+        if ($_POST) {
+            $resposta = $dao->verificaLogin($_POST['ra'], $_POST['senha']);
+            var_dump($_POST['ra']);
+            die();
+
+            if ($resposta != '' and $resposta != 'NULL') {
+                session_start();
+                $_SESSION['aluno'] = $resposta->getIDAluno();
+                $this->render('index');
+                exit();
+            } else {
+                $this->view->message = 'RA ou senha Incorretos';
+            }
+        }
+    }
 }
